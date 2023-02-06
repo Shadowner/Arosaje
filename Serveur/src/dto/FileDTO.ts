15a29593af739cac8da3cbe90c2217ec5bae325b
Shadowner <12647984+Shadowner@users.noreply.github.com>
2023-02-06 14:24:40 +0100

    Supression dossier DTO

--- Serveur/src/dto/FileDTO.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity } from "./BaseEntity";
-import { UserDTO } from "./UserDTO";
-
-export interface FileDTO extends BaseEntity {
-    url: string,
-    owner: UserDTO,
-    shared: UserDTO[],
-    name:string,
-}
--- Serveur/src/dto/RoleDTO.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEntity } from "./BaseEntity";
-
-export interface RoleDTO extends BaseEntity {
-    name:string,
-    flags:string[]
-}
