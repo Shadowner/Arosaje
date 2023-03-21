4d217b80d8c019e6056a41f700a511c4d15cf0b1
Ahmosys <ahmosyspro@protonmail.com>
2023-03-21 09:52:07 +0100

    feat: add .env variable for security

--- Serveur/src/config/data-source.ts
+++ Serveur/src/config/data-source.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import dotenv from "dotenv";
 import { Plant } from "../models/plant.model";
 import { Role } from "../models/role.model";
 import { User } from "../models/user.model"
@@ -10,13 +11,15 @@ import { Session } from "../models/session.model";
 import { Conversation } from "../models/conversation.model";
 import { Message } from "../models/message.model";
 
+dotenv.config();
+
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "",
-  database: "mspr_dev",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [User, Role, Plant, File, PlantType, Guard, Session, Conversation, Message],
