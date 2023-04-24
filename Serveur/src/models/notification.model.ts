005a433b3a0d843379486228a4bb93d22bd5b5b5
Ahmosys <ahmosyspro@protonmail.com>
2023-04-24 09:20:35 +0200

    feat: add notification model and edit user model to fit with

---
+++ Serveur/src/models/notification.model.ts
@@ -0,0 +1,23 @@
+import { IsDate } from 'class-validator';
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.model';
+
+@Entity()
+export class Notification extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  content!: string;
+  
+  @Column()
+  @IsDate()
+  sendDate!: Date;
+
+  @Column({ default: false })
+  isRead!: Date;
+
+  @ManyToOne(() => User, user => user.notifications, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user!: User;
+}
