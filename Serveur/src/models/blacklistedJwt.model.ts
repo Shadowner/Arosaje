bb04ed4d5a6970cfd797ca96850618e39b0198e7
Ahmosys <ahmosyspro@protonmail.com>
2023-04-25 13:41:53 +0200

    feat: add new model

---
+++ Serveur/src/models/blacklistedJwt.model.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class BlacklistedJwt extends BaseEntity {
+
+    @PrimaryColumn()
+    token!: string;
+
+    @Column()
+    expirationDate!: Date;
+
+    public toObject() {
+        return {
+            token: this.token,
+            expirationDate: this.expirationDate,
+        }
+    }
+}
