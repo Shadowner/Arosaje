947c2a6835cb3a4f405989d46939b6fb9b4af4d8
Ahmosys <ahmosyspro@protonmail.com>
2023-05-10 22:03:26 +0200

    feat: add GuardController tests

---
+++ Serveur/__tests__/controllers/guard.controller.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+
+const baseURL = "http://localhost:8080"
+
+describe("GuardController", () => {
+  let userTokenJWT: string;
+
+  beforeAll(async () => {
+    const credentials = { email: "user@localhost", password: "123456789" };
+    const response = await request(baseURL).post("/user/login").send(credentials);
+    userTokenJWT = response.body.jwt;
+  });
+
+  describe("GET /guard/start/:id", () => {
+    
+  }); 
+});
